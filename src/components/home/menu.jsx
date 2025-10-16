280f996f41f7a371135fd376557bd1c3dd06720e
Meghan <afigueroa57@ucol.mx>
2025-10-16 00:38:32 -0600

    Eliminación de la barra de busqueda de home

--- src/components/home/menu.jsx
+++ src/components/home/menu.jsx
@@ -10,16 +10,6 @@ function Menu() {
                 <li><a href="#" className="hover:underline">Blog</a></li>
             </ul>
 
-            {/* Middle - Search bar */}
-            <div className="flex items-center flex-1 mx-6">
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="w-full rounded-full border border-gray-300 px-4 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
-                />
-
-            </div>
-
             {/* Right side - Login icon */}
             <div className="flex items-center">
                 <button className="p-2 hover:bg-gray-100 rounded-full">
